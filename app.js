50cfc7d55ab6356b4fe7f88ba986c8ffda2e4025
Fernando <ferdelamad@gmail.com>
2018-04-26 10:04:29 -0700

    updated version

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 /*
 GAME RULES:
-
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 var scores, roundScore, activePlayer, gamePlaying;
 
+var lastDice;
+var counterSix = 0;
 //Start the Game
 init();
 
@@ -24,15 +24,24 @@ document.querySelector(".btn-roll").addEventListener('click', function() {
     dicePic.style.display = 'unset';
     dicePic.src = 'dice-' + dice + '.png';
 
-    //3.- Update the Round score IF the number is NOT a 1
-    if (dice !== 1) {
+    if (dice === 6 && lastDice === 6) {
+      console.log('You got a second 6 =/')
+      scores[activePlayer] = 0;
+      document.getElementById('score-' + activePlayer).textContent = 0;
+      nextPlayer();
+    } else if (dice !== 1) { //3.- Update the Round score IF the number is NOT a 1
       //add score
+      if (dice === 6) {
+        console.log('You got one 6 wath out for the second one!')
+        counterSix++;
+      }
       roundScore += dice
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
       //next player
     nextPlayer();
     }
+    lastDice = dice;
   }
 });
 
@@ -65,6 +74,8 @@ document.querySelector('.btn-new').addEventListener('click', init);
 function nextPlayer() {
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   roundScore = 0;
+  lastDice = 0;
+  counterSix = 0;
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
   document.querySelector('.player-0-panel').classList.toggle('active');
